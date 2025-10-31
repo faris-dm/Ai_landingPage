10745eade1da26d823dd12ab1c42ff106fa781c7
faris Naser <solonaser9@gmail.com>
2025-10-31 10:56:39 -0700

    frist commi

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Coffee } from "lucide-react";
+
+function App() {
+  return (
+    <>
+      <div className=" flex gap-2">
+        <div className="text-red-400">app one</div>
+        <Coffee />
+      </div>
+    </>
+  );
+}
+
+// Credential Manager
+
+export default App;
