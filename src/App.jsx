8debf18c491faaeffe2bf29d947a52ead8d6a1f0
faris Naser <solonaser9@gmail.com>
2025-11-01 02:06:12 -0700

    final style in the  options

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,68 @@ import {
   Rocket,
   CodeXml,
 } from "lucide-react";
+import { div } from "motion/react-client";
 
 function App() {
   return (
     <>
       <div className="">
         <Landing />
-        <div className="flex justify-center gap-2  flex-wrap p-3 ">
-          <div className="grid grid-cols-3 gap-4 ">
-            <Box icons={<ListCheck />} text="Show my task" />
-            <Box icons={<Plus />} text="My  Total Star" />
-            <Box icons={<GitPullRequest />} text="My  Total Star" />
-            <Box icons={<Users />} text="My  Total Star" />
-            <Box icons={<Rocket />} text="My  Total Star" />
-            <Box icons={<CodeXml />} text="My  Total Star" />
+        <div className="flex justify-center  flex-wrap p-3 ">
+          <div className="grid grid-cols-3 gap-5   ">
+            <Box
+              icons={
+                <div className="bg-[#19BF6E] p-1 rounded">
+                  <ListCheck className="text-white" />
+                </div>
+              }
+              text="Show my task"
+            />
+            <Box
+              icons={
+                <div className="bg-[#10ACDA] p-1 rounded">
+                  <Plus className="text-white" />
+                </div>
+              }
+              text="Create New Task"
+            />
+            <Box
+              icons={
+                <div className="bg-[#F55F29] p-1 rounded">
+                  <GitPullRequest className="text-white" />
+                  {/* #DA4CB2 */}
+                </div>
+              }
+              text="PR Review"
+            />
+            <Box
+              icons={
+                <div className="bg-[#DA4CB2] rounded p-1">
+                  <Users className="text-white " />
+                  {/* #F48811 */}
+                </div>
+              }
+              text="Team Status"
+            />
+
+            <Box
+              icons={
+                <div className="bg-[#2794E9] p-1 rounded">
+                  <Rocket className="text-white " />
+                  {/* ##2794E9 */}
+                </div>
+              }
+              text="Deploy Status"
+            />
+            <Box
+              icons={
+                <div className="bg-[#2794E9] p-1 rounded">
+                  <CodeXml className="text-white  " />
+                  {/* ##2794E9 */}
+                </div>
+              }
+              text="Code"
+            />
           </div>
         </div>
       </div>
--- src/pages/Box.jsx
+++ src/pages/Box.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Box({ icons, text }) {
   return (
-    <div className="  flex justify-center ">
-      <div className=" flex items-center gap-2 w-50 p-5 rounded border border-red-200 bg-transparent text-[#fff] ">
-        <div className="">{icons}</div>
-        <p>{text}</p>
+    <div className="flex justify-center ">
+      <div className="flex items-center gap-3 w-60 px-2 py-4 rounded-xl  bg-[#31344F] text-[#fff]  ">
+        <div className=" rounded-xl">{icons}</div>
+        <p className="text-xs font-bold">{text}</p>
       </div>
     </div>
   );
