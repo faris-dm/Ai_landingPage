a57ad4c4af91f29c96ba3530069d3cbf01acea85
faris Naser <solonaser9@gmail.com>
2025-11-05 18:27:40 -0800

    placing  the inviddeo image in the cneter

--- src/pages/page_two/Ai_trends.jsx
+++ src/pages/page_two/Ai_trends.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-
+import ImageOne from "./images/image.png";
 function Ai_trends({ Seller }) {
   return (
     <>
-      <div>
-        <h3> i am solo</h3>
-        {/* <div>
-          {Seller.map((img, index) => (
-            <img key={index} src={img} alt="No Problem" />
-          ))}
-        </div> */}
-        {/* <h2>{heading}</h2>
-        <h5>{text}</h5>
-        <p>{link}</p> */}
+      <div className=" p-3  flex items-center justify-between mx-auto   bg-black   p-7">
+        <div className=" mx-auto  w-20 h-20 rounded-full object-cover bg-white">
+          <img
+            width={100}
+            height={100}
+            className=""
+            sizes={20}
+            src={ImageOne}
+            alt=""
+          />
+        </div>
+        <div className="mx-auto">
+          <h3>Video Ai by Invideo</h3>
+          <p> 4 🐱‍💻 Ai video maker using sor 2</p>
+          <a href=""> By Invideo Ai</a>
+        </div>
       </div>
     </>
   );
